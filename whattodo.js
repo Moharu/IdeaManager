a10092d38d46891eceffe6730388c02e7be5a796
João Teló <joaotelo.nh@hotmail.com>
2015-01-14 13:43:52 -0200

    Função de login testada, consulta no DB

--- whattodo.js
+++ whattodo.js
@@ -26,3 +26,27 @@ function recData(){
     xmlhttp2.open("GET","listproblems.php",true);
     xmlhttp2.send();
 }
+
+function login(){
+	document.getElementById("accountForm").innerHTML = "<input id='p02' placeholder='Login'/><input id='p03' placeholder='Senha' /><button id='oklogin' onclick='requestLogin()'>Ok</button>";     
+}
+
+function requestLogin(){
+	login = document.getElementById("p02").value;
+	pw = document.getElementById("p03").value;
+	xmlhttp3 = new XMLHttpRequest();
+	xmlhttp3.onreadystatechange = function (){
+		if(xmlhttp3.readyState == 4 && xmlhttp3.status == 200) {
+			//RESOLUCAO
+			document.getElementById("verification").innerHTML = xmlhttp3.responseText;
+			if(document.getElementById("verification").innerHTML == 1){
+				document.getElementById("txtHint").innerHTML = "Deu";
+			}
+			else{
+				document.getElementById("txtHint").innerHTML = "Não deu";
+			}
+		}
+	}
+	xmlhttp3.open("GET","loginvalidation.php?l="+login+"&p="+pw, true);
+	xmlhttp3.send();
+}
